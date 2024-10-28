368aa19d8bfd60100fa178ef5f175e2633d498c0
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2024-10-28 19:37:24 -0300

    connection path

--- api_restful/index.js
+++ api_restful/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const connection = require("./database/connection");
+const connection = require("../database/connection");
 const app = express();
 
 app.use(cors());
